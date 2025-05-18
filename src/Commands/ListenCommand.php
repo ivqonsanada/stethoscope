7a33219be00a78005b2b041cd08f744a836843b0
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2025-05-18 15:21:29 +0700

    feat: update commands classes with private attributes

--- src/Commands/ListenCommand.php
+++ src/Commands/ListenCommand.php
@@ -14,90 +14,98 @@ use MohsenAbrishami\Stethoscope\Traits\MessageCreatorTrait;
 
 class ListenCommand extends Command
 {
-    use MessageCreatorTrait;
+  use MessageCreatorTrait;
 
-    public function __construct(Cpu $cpu, Memory $memory, Network $network, WebServer $webServer, StorageService $storage)
-    {
-        parent::__construct();
+  private Cpu $cpu;
+  private Memory $memory;
+  private Network $network;
+  private WebServer $webServer;
+  private StorageService $storageService;
+  private \Illuminate\Contracts\Filesystem\Filesystem $storage;
 
-        $this->cpu = $cpu;
-        $this->memory = $memory;
-        $this->network = $network;
-        $this->webServer = $webServer;
-        $this->storageService = $storage;
 
-        $this->storage = Storage::disk(config('stethoscope.storage.driver'));
+  public function __construct(Cpu $cpu, Memory $memory, Network $network, WebServer $webServer, StorageService $storage)
+  {
+    parent::__construct();
+
+    $this->cpu = $cpu;
+    $this->memory = $memory;
+    $this->network = $network;
+    $this->webServer = $webServer;
+    $this->storageService = $storage;
+
+    $this->storage = Storage::disk(config('stethoscope.storage.driver'));
+  }
+
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'stethoscope:listen {resources?*} {--notif}';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'monitor memory usage, cpu usage, network connection and nginx status';
+
+  /**
+   * Execute the console command.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    $resources = collect($this->argument('resources'));
+
+    $resourcesIsEmpty = $resources->isEmpty();
+
+    $logs = [];
+    $logs['signature'] = $this->signature;
+
+    $this->line(
+      $this->timeMessage()
+    );
+
+    if ($resources->contains('cpu') || $resourcesIsEmpty) {
+      $logs['cpu'] = $this->cpu->check();
+      $this->info(
+        $this->cpuMessage($logs['cpu'])
+      );
     }
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'stethoscope:listen {resources?*} {--notif}';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'monitor memory usage, cpu usage, network connection and nginx status';
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        $resources = collect($this->argument('resources'));
-
-        $resourcesIsEmpty = $resources->isEmpty();
-
-        $logs = [];
-        $logs['signature'] = $this->signature;
-
-        $this->line(
-            $this->timeMessage()
-        );
-
-        if ($resources->contains('cpu') || $resourcesIsEmpty) {
-            $logs['cpu'] = $this->cpu->check();
-            $this->info(
-                $this->cpuMessage($logs['cpu'])
-            );
-        }
-
-        if ($resources->contains('memory') || $resourcesIsEmpty) {
-            $logs['memory'] = $this->memory->check();
-            $this->info(
-                $this->memoryMessage($logs['memory'])
-            );
-        }
-
-        if ($resources->contains('network') || $resourcesIsEmpty) {
-            $logs['network'] = $this->network->check();
-            $this->info(
-                $this->networkMessage($logs['network'])
-            );
-        }
-
-        if ($resources->contains('web-server') || $resourcesIsEmpty) {
-            $logs['webServer'] = $this->webServer->check();
-            $this->info(
-                $this->webServerMessage($logs['webServer'])
-            );
-        }
-
-        if ($resources->contains('storage') || $resourcesIsEmpty) {
-            $logs['storage'] = $this->storageService->check();
-            $this->info(
-                $this->storageMessage($logs['storage'])
-            );
-        }
-        
-        if ($this->option('notif')){
-            TroubleOccurred::dispatch($logs);
-        }
+    if ($resources->contains('memory') || $resourcesIsEmpty) {
+      $logs['memory'] = $this->memory->check();
+      $this->info(
+        $this->memoryMessage($logs['memory'])
+      );
     }
+
+    if ($resources->contains('network') || $resourcesIsEmpty) {
+      $logs['network'] = $this->network->check();
+      $this->info(
+        $this->networkMessage($logs['network'])
+      );
+    }
+
+    if ($resources->contains('web-server') || $resourcesIsEmpty) {
+      $logs['webServer'] = $this->webServer->check();
+      $this->info(
+        $this->webServerMessage($logs['webServer'])
+      );
+    }
+
+    if ($resources->contains('storage') || $resourcesIsEmpty) {
+      $logs['storage'] = $this->storageService->check();
+      $this->info(
+        $this->storageMessage($logs['storage'])
+      );
+    }
+
+    if ($this->option('notif')) {
+      TroubleOccurred::dispatch($logs);
+    }
+  }
 }
